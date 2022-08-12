c3f20ceca108d3e14058665f39065bf33104693b
Viktor A <abcd999@bk.ru>
2022-08-13 01:18:49 +0300

    Задача 10 - первый способ

---
+++ task_10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показывает вторую цифру трёхзначного числа
+
+beginning:
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (Math.Abs(number) > 99 && Math.Abs(number) < 1000)
+{
+    Console.Write(Math.Abs((number%100 - number%10)/10));
+}
+else
+{
+    goto beginning;
+}
+
