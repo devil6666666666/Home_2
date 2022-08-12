173cd0cd2619330a6b7b721a4e405d4159fe2ef9
Viktor A <abcd999@bk.ru>
2022-08-13 02:09:03 +0300

    Задача 10 - второй способ

---
+++ task_10_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показывает вторую цифру трёхзначного числа
+
+beginning:
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (Math.Abs(number) > 99 && Math.Abs(number) < 1000)
+{
+   string str = Convert.ToString(Math.Abs(number));
+   char [] ar = str.ToCharArray();
+   Console.WriteLine(ar[1]);
+
+}
+else
+{
+    goto beginning;
+}
