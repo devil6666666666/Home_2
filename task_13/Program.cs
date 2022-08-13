4214f5064add6283c9a9d98dc691b156a548d696
Viktor A <abcd999@bk.ru>
2022-08-13 20:29:13 +0300

    Задача 15

---
+++ task_13/Program.cs
@@ -0,0 +1,23 @@
+﻿// Принимает на вход номер дня недели и проверяет, 
+// является ли он выходным
+
+Console.Write("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string[] arr = { "нет", "да" };
+if (number >= 1 && number <= 7)
+{
+    if (number < 6)
+    {
+        Console.WriteLine(arr[0]);
+    }
+    else
+    {
+        Console.WriteLine(arr[1]);
+    }
+
+}
+else
+{
+    Console.WriteLine("Такого дня нет");
+}
+
