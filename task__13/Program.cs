e4689ac484581b2c5f2baa52c8c43389ad8b0ac0
Viktor A <abcd999@bk.ru>
2022-08-13 21:07:01 +0300

    Задача 13

---
+++ task__13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int module = Math.Abs(number);
+string str = Convert.ToString(module);
+char[] ar = str.ToCharArray();
+int length = ar.Length;
+if (length > 2)
+{
+    Console.WriteLine(ar[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
